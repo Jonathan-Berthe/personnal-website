802a3c263ff82db8cd60dda97c69e2e5c92c5e48
Bonistorm <jonath.berthe@gmail.com>
2021-02-24 18:46:20 +0100

    cv feature

--- components/Section1.js
+++ components/Section1.js
@@ -1,19 +1,33 @@
 import styles from '../styles/Section1.module.scss'
 import Image from 'next/image'
 
+import LinkSVG from '../public/link.svg'
+
 export default function Section1() {
+
+  const handleClick = () => {
+    window.open('/CV_Berthe.pdf')
+  }
   return (
     <div className={styles.section1 + ' section'} id='section1'>
       <div className={styles.section1Content}>
         <div className={styles.titleContainer}>
           <h1>Jonathan Berthe</h1>
           <h2>Front-end developer &amp; Engineer</h2>
+          <div className={styles.cvContainer} onClick={handleClick}>
+            <LinkSVG className={styles.cvSvg}>
+
+            </LinkSVG>
+            <p>
+              View my resume
+            </p>
+          </div>
         </div>
         <div className={styles.spacer2}></div>
         <div className={styles.portraitContainer}>
           <div className={styles.imgContainer}>
             <Image
-              src="/../public/me.jpg"
+              src="/me.jpg"
               alt="Picture of the author"
               layout='fill'
               objectFit='cover'
@@ -38,7 +52,6 @@ export default function Section1() {
               priority={true}
             />
           </div>
-
         </div>
         <div className={styles.spacer4}></div>
       </div>
