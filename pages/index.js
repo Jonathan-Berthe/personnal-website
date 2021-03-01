98f05f40d304c9919f29449d8942d38ca4ceba5c
Bonistorm <jonath.berthe@gmail.com>
2021-03-01 17:27:50 +0100

    improve things

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import Section1 from '../components/Section1'
 import Section2 from '../components/Section2'
 import SectionContact from '../components/SectionContact'
